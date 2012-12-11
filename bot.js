9459185ccfc493946d95bb42a3b08f93ac681a89
David Chen <chenosaurus@gmail.com>
2012-12-11 01:01:04 -0800

    changed to rpi hardware uart

--- bot.js
+++ bot.js
@@ -3,13 +3,13 @@ var express     = require("express"),
     serialport  = require("serialport"),
     SerialPort = serialport.SerialPort; 
 
-// var bot = new SerialPort("/dev/ttyACM0", {
-//   baudrate: 9600
-// });
-
-var bot = new SerialPort("/dev/tty.usbmodemfa131", {
-  baudrate: 9600
+var bot = new SerialPort("/dev/ttyAMA0", {
+   baudrate: 9600 
 });
+
+//var bot = new SerialPort("/dev/tty.usbmodemfa131", {
+ // baudrate: 9600
+//});
 
 bot.on("data", function (data) {
   console.log(data.toString());
@@ -46,6 +46,16 @@ app.get('/command/f', function(req, res){
 app.get('/command/b', function(req, res){
   res.send('ok');
   backward();
+});
+
+app.get('/command/l', function(req, res){
+  res.send('ok');
+  left();
+});
+
+app.get('/command/r', function(req, res){
+  res.send('ok');
+  right();
 });
 
 app.get('/command/s', function(req, res){

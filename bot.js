b4b4be05ef88ceed70b9e37cac5641c2b9b8f114
David Chen <chenosaurus@gmail.com>
2012-12-10 22:55:51 -0800

    updates

--- bot.js
+++ bot.js
@@ -8,14 +8,33 @@ var express     = require("express"),
       baudrate: 9600
     });
 
+function forward() {
+  bot.write('f');
+}
+
+function backward() {
+  bot.write('b');
+}
+
+function left() {
+  bot.write('l');
+}
+
+function right() {
+  bot.write('r');
+}
+
+function stop() {
+  bot.write('s');
+}
 
 app.use(express.static(__dirname + '/public'));
 
 io.sockets.on('connection', function (socket) {
   socket.on('drive', function (data) {
     console.log(data);
-    if (data.command) {
-      bot.write(data.command);
+    if (data.command == "l") {
+      left();
     }
   });
 });

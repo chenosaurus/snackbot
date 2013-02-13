f2094584d2eb374aedbae0aad918e83974f2c758
David Chen <chenosaurus@gmail.com>
2013-02-12 17:51:29 -0800

    update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mjpg_streamer -b -i "/usr/local/lib/input_uvc.so -n -f 15 -r 640x480" -o "/usr/local/lib/output_http.so -n -p 8000"
+mjpg_streamer -b -i "/usr/local/lib/input_uvc.so -n -f 3 -r 320x240" -o "/usr/local/lib/output_http.so -n -p 8000"
